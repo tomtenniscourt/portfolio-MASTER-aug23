1160ebbc405e0e5cb6c79f5d62e6ecf5ace99b7d
Tom_Court <tomcourt94@icloud.com>
2023-09-19 16:53:24 +0100

    s44

--- src/App.js
+++ src/App.js
@@ -181,7 +181,7 @@ function App() {
         <div className="full-page">
           <div id="section1" className="section">
             <div className="home-content">
-              <h1 className="home-h1">Tom Court: Software Engineer</h1>
+              <h1 className="home-h1">Tom Court xx: Software Engineer</h1>
               <p className="home-p"></p>
               <img
                 className="picture"
